a15fa6615a6969523387b18a553cad0e505945d8
codingwithking <72618480+codingwithking@users.noreply.github.com>
2021-01-27 21:59:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CounterOne from "./CounterOne";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CounterOne.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CounterOne() {
+  return <h1>CounterOne</h1>;
+}
+
+export default CounterOne;
