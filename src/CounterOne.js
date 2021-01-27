c341b405633b6a53239b455f6e67f7c87940d77a
codingwithking <72618480+codingwithking@users.noreply.github.com>
2021-01-27 22:26:47 +0530

    Added Fake API JSON holder in Useeffect

--- src/CounterOne.js
+++ src/CounterOne.js
@@ -1,7 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function CounterOne() {
-  return <h1>CounterOne</h1>;
+  const [countValues, setCountValues] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setCountValues(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  return (
+    <ul>
+      {countValues.map((item) => {
+        return (
+          <li key={item.id}>
+            <div>
+              <h4>Title: {item.title}</h4>
+              <h6>userId:{item.userId}</h6>
+            </div>
+          </li>
+        );
+      })}
+      {JSON.stringify(countValues)}
+    </ul>
+  );
 }
 
 export default CounterOne;
